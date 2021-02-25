2b36c52f2d412e9185a074320936045ac26c7920
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-02-25 10:35:00 -0500

    Basic Statements

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -1,4 +1,12 @@
 USE codeup_test_db;
 
 INSERT INTO albums (artst, name, release_date, sales, genre)
-VALUES ('Michael Jackson', 'Thriller', 1982, '47,300,000', 'Pop, post-disco, funk, rock');
+VALUES ('Michael Jackson', 'Thriller', 1982, '47,300,000', 'Pop, post-disco, funk, rock'),
+       ('AC/DC', 'Back in Black', 1980, '29,400,000', 'Hard rock'),
+       ('Meat Loaf', 'Bat Out of Hell', 1977, '21,700,000', 'Hard rock, glam rock, progressive rock'),
+       ('Pink Floyd', 'Dark Side of the Moon', 1973, '24,400,000', 'Progressive rock'),
+       ('Whitney Houston', 'The Bodyguard', 1992, '28,400,000', 'R&B, soul, pop, soundtrack'),
+       ('Eagles', 'Their Greatest Hits (1971-1975)', 1976, '41,200,000', 'Country rock, soft rock, folk rock'),
+       ('Bee Gees', 'Saturday Night Fever', 1977, '21,600,000', 'Disco'),
+       ('Fleetwood Mac', 'Rumours',	1977, '27,900,000',	'Soft rock'),
+       ('Shania Twain', 'Come On Over', 1997, '29,600,000', 'Country, pop');
