e8173bfb18ab7e19d6aebacabf6ede166a757136
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-03-01 13:16:48 -0500

    JOIN exercises

--- join_exercises.sql
+++ join_exercises.sql
@@ -21,3 +21,18 @@ FROM employees AS e
               ON d.dept_no = de.dept_no
 WHERE dm.to_date = '9999-01-01' AND e.gender = 'F'
 ORDER BY d.dept_name;
+
+SELECT t.title AS 'Title', COUNT(*) AS 'Count'
+FROM employees AS e
+    JOIN titles AS t
+        ON e.emp_no = t.emp_no
+    JOIN dept_emp de
+        ON e.emp_no = de.emp_no
+    JOIN departments d
+        ON de.dept_no = d.dept_no
+    JOIN dept_manager dm on d.dept_no = dm.dept_no
+WHERE d.dept_name = 'Customer Service' AND
+      t.to_date = '9999-01-01' AND
+      dm.to_date = '9999-01-01' AND
+      de.to_date = '9999-01-01'
+GROUP BY t.title;
