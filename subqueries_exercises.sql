a544d4e33927f5afa87aab5316dbbff961196a28
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-03-01 17:45:44 -0500

    Subqueries exercises

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -7,3 +7,11 @@ WHERE e.hire_date IN (
     FROM employees e
     WHERE e.emp_no = 101010
     );
+
+SELECT t.title AS 'Titles'
+FROM titles t
+WHERE t.emp_no IN (
+    SELECT e.emp_no
+    FROM employees e
+    WHERE e.first_name = 'Aamod'
+    );
