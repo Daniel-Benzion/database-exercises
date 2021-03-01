c4ab78e5d5b4b2b1b4a8ddc7aba46187867558c4
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-03-01 13:01:03 -0500

    JOIN exercises

--- join_exercises.sql
+++ join_exercises.sql
@@ -10,3 +10,14 @@ JOIN departments d
     ON d.dept_no = de.dept_no
 WHERE dm.to_date = '9999-01-01'
 ORDER BY d.dept_name;
+
+SELECT d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager'
+FROM employees AS e
+         JOIN dept_emp AS de
+              ON e.emp_no = de.emp_no
+         JOIN dept_manager AS dm
+              ON e.emp_no = dm.emp_no
+         JOIN departments d
+              ON d.dept_no = de.dept_no
+WHERE dm.to_date = '9999-01-01' AND e.gender = 'F'
+ORDER BY d.dept_name;
