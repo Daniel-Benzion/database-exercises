1131aa680e3cd8c3b2ccc10893c3747f75d95e8b
Daniel Benzion <daniel.z.benzion@gmail.com>
2021-03-01 13:31:40 -0500

    JOIN exercises

--- join_exercises.sql
+++ join_exercises.sql
@@ -36,3 +36,16 @@ WHERE d.dept_name = 'Customer Service' AND
       dm.to_date = '9999-01-01' AND
       de.to_date = '9999-01-01'
 GROUP BY t.title;
+
+SELECT d.dept_name AS 'Department Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Name', s.salary AS 'Salary'
+FROM employees AS e
+         JOIN salaries s
+             ON e.emp_no = s.emp_no
+         JOIN dept_emp AS de
+              ON e.emp_no = de.emp_no
+         JOIN dept_manager AS dm
+              ON e.emp_no = dm.emp_no
+         JOIN departments d
+              ON d.dept_no = de.dept_no
+WHERE dm.to_date = '9999-01-01' AND s.to_date = '9999-01-01'
+ORDER BY d.dept_name;
